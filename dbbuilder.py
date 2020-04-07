72f21dfcc048cf53f9e3a3991d3a889fd23359d7
jourm <jourm91@gmail.com>
2020-04-07 10:30:14 +0000

    Add founction to add the recived data to the database in dbbuilder.py

--- dbbuilder.py
+++ dbbuilder.py
@@ -1,5 +1,15 @@
 import requests
 import json
+import pymongo
+from bson.objectid import ObjectId
+import os
+from os import path
+if path.exists("env.py"):
+    import env
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DBS_NAME = "TheCocktailCollection"
+COLLECTION_NAME = "ingredients"
 
 
 def getnewingredient(id):
@@ -15,3 +25,21 @@ def getnewingredient(id):
                "alcohol": value[0]['strAlcohol'],
                "abv": value[0]['strABV']}
 
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is COnnected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could Not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+for i in range(1, 600):
+    new_ingredient = getnewingredient(i)
+
+    coll.insert_one(new_ingredient)
