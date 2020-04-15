e77ea30718b6b2053d6d6dbdeb479dbe2b1166af
jourm <jourm91@gmail.com>
2020-04-15 09:55:40 +0000

    change port and ip

--- app.py
+++ app.py
@@ -223,6 +223,6 @@ def register():
 
 if __name__ == '__main__':
     app.secret_key = os.environ.get('SECRET')
-    app.run(host=0.0.0.0,
+    app.run(host='0.0.0.0',
             port=5000,
             debug=True)
