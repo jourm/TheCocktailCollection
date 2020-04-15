800f2073e49c9a67d77aaee7f2164190e5ac65d0
jourm <jourm91@gmail.com>
2020-04-15 09:29:06 +0000

    heroku version

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["MONGO_URI"] = "mongodb+srv://root:N05p00n64@thecocktailcoleection-gvsu0.mongodb.net/TheCocktailCollection?retryWrites=true&w=majority"
+os.environ["SECRET"] = "SAkCiweUqpy0gEuV9q3AmDt8Bybry38G"
