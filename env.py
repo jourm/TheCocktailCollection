d63a2e1dd6fc9e6a6caed90a54dee632b6642b99
jourm <jourm91@gmail.com>
2020-04-15 11:40:55 +0000

    start readme

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_URI"] = "mongodb+srv://root:N05p00n64@thecocktailcoleection-gvsu0.mongodb.net/TheCocktailCollection?retryWrites=true&w=majority"
-os.environ["SECRET"] = "SAkCiweUqpy0gEuV9q3AmDt8Bybry38G"
